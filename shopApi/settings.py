d5b8df1dbd6dd0d816d849217840dbafaf8c679e
Viorel Moldovan <viorelmoldovan@gmail.com>
2023-03-03 14:45:18 +0200

    vercel n3

--- shopApi/settings.py
+++ shopApi/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    #'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
     'fashionStore',
 ]
